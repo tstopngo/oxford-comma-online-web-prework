81afb896ee857a6ac15c3c9c7ed6715aef7df05c
Tracy Ngo <tngo1206@gmail.com>
2019-02-09 07:56:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array[0]
+  elsif array.length == 2
+    last_item =" and " + array.pop
+    array.push(last_item)
+    array.join
+  else
+    last_item ="and " + array.pop
+    array.push(last_item)
+    array.join(", ")
+  end
 end
